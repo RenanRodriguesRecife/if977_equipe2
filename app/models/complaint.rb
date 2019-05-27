a0756538822494e6a7bab546d4ae10c801b37c22
Pedro <phac@cin.ufpe.br>
2019-05-27 20:19:28 +0000

    Add user profile html

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,5 +1,6 @@
 class Complaint < ApplicationRecord
   belongs_to :user
+  has_many :comment
   mount_uploader :photo, LocationPhotoUploader
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :complaint
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :complaint
 end
