05250b40ad65e315615719ea39304810a9d79b81
matlheus <matheusagome@gmail.com>
2019-05-29 20:43:05 +0000

    dependent destroy

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,7 +1,7 @@
 class Complaint < ApplicationRecord
   belongs_to :user
-  has_many :comment
-  has_many :like, dependent: :destroy
+  has_many :comment, dependent: :destroy
+  has_many :like
   mount_uploader :photo, LocationPhotoUploader
   
 end
