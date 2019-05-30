6d326471504b57d3938ab9dd6e6fd9028a5596f1
Pedro <phac@cin.ufpe.br>
2019-05-30 04:15:01 +0000

    Update user registration front end and Add image preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,21 @@
 //= require bootstrap
 
 //= require bootstrap/modal
+
+$(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#img_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#avatar-upload").change(function(){
+    $('#img_prev').removeClass('hidden');
+    readURL(this);
+  });
+});
