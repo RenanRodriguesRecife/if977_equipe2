fb0859a46832c4c6180c3c7298b132d59c042fcb
Pedro <phac@cin.ufpe.br>
2019-06-16 19:31:24 +0000

    Add permissions to non admin users and admin users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,34 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+  
+    if user.admin?
+        can :manage, :all
+    else
+        can :update, Comment do |comment|
+            comment.user == user
+        end
+        
+        can :update, Complaint do |complaint|
+            complaint.user == user
+        end
+        
+        can :destroy, Like do |like|
+            like.user == user
+        end
+        
+        can :destroy, Comment do |comment|
+            comment.user == user
+        end
+        
+        can :destroy, Complaint do |complaint|
+            complaint.user == user
+        end
+    
+        can :create, Comment
+        can :create, Complaint
+        can :create, Like
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
